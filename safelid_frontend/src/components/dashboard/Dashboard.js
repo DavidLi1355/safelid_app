5fa557654be22caec3d79f8b31e498a872685a74
DavidLi1355 <lid1355218215@outlook.com>
2020-08-10 22:25:19 -0700

    fixed layout issue on phone screen

--- safelid_frontend/src/components/dashboard/Dashboard.js
+++ safelid_frontend/src/components/dashboard/Dashboard.js
@@ -38,9 +38,9 @@ class Dashboard extends Component {
                     <Route path='/dashboard/folder'>
                         <DashboardNavBar history={history}/>
                         <div className='container-fluid d-flex flex-column vh-100'>
-                            <div className='row flex-grow-1'>
-                                <div className='col-sm-3 col-md-2 mh-100 py-2'><ItemSidebar /></div>
-                                <div className='col-sm-9 col-md-10 mh-100 '><ItemContainer history={history}/></div>
+                            <div className='row flex-grow'>
+                                <div className='col-sm-3 col-md-2 pb-4'><ItemSidebar /></div>
+                                <div className='col-sm-9 col-md-10 '><ItemContainer history={history}/></div>
                             </div>
                         </div>
                     </Route>
